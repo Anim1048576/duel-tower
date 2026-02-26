207e4cddd574d6a276b44885732150e1afb5853c
Anim1048576 <darekax10@gmail.com>
2026-02-26 10:06:54 +0900

    asdf

--- duel-tower-ui/vite.config.ts
+++ duel-tower-ui/vite.config.ts
@@ -4,4 +4,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig(({ mode }) => ({
   plugins: [svelte()],
   base: mode === 'production' ? '/ui/' : '/',
+  server: {
+    proxy: {
+      '/api': 'http://localhost:9009',
+      '/ws': { target: 'ws://localhost:9009', ws: true },
+    },
+  },
+  build: {
+    outDir: '../src/main/resources/static/ui',
+    emptyOutDir: true,
+  },
 }))
