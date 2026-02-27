93f067b1df6699aafe0745bc070a518e24d68baf
Anim1048576 <darekax10@gmail.com>
2026-02-27 14:25:30 +0900

    asdf

---
+++ src/main/java/com/example/dueltower/engine/model/CombatPhase.java
@@ -0,0 +1,12 @@
+package com.example.dueltower.engine.model;
+
+public enum CombatPhase {
+    INIT,
+    ROUND_START,
+    TURN_START,
+    MAIN,
+    TURN_END,
+    ROUND_END,
+    CHECK_VICTORY,
+    END
+}
